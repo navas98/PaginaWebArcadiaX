6a07696a776e9e34ef506a33b2ba1472e394af7a
Javier Navas <j.navasdam@gmail.com>
2025-12-28 00:17:45 +0100

    Modificacion archivos vacios

---
+++ app/projects/arcade/page.tsx
@@ -0,0 +1,7 @@
+export default function ArcadePage() {
+  return (
+    <div>
+      <h1>Arcade</h1>
+    </div>
+  );
+}
---
+++ app/projects/multimedia/page.tsx
@@ -0,0 +1,7 @@
+export default function MultimediaPage() {
+  return (
+    <div>
+      <h1>Multimedia</h1>
+    </div>
+  );
+}
